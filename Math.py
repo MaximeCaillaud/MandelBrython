e07b2dbf006e8426f3484f5cb78ff880e80cdc66
Maxime <maxime.caillaud93@gmail.com>
2022-06-17 20:54:48 +0200

    Presque

--- Math.py
+++ Math.py
@@ -1,16 +1,40 @@
+import sys
+
 from PIL import Image
+import json
 
 
-def pointc(ind=1):
+def pointc(indy,indx):
     tabc = []
-    x = 2.001
-    while x > -ind:
-        x = round(x - 0.001, 3)
-        y = 2.001
-        while y > -ind:
-            y = round(y - 0.001, 3)
-            tabc.append((x, y))
+    x = -2.0001
+    while x < indx:
+        x = round(x + 0.0001, 4)
+        y = -2.0001
+        while y < indy:
+            y = round(y + 0.0001, 4)
+            tabc.append((int(x*1000+2000), int(y*1000+2000)))
     return tabc
+
+
+def pointcvalide(ind=1):
+    cx = 2.0001
+    while cx > -ind:
+        cx = round(cx - 0.0001, 4)
+        cy = 2.0001
+        while cy > -ind:
+            cy = round(cy - 0.0001, 4)
+            x = 0
+            y = 0
+            i = 0
+            while distom(x, y) < 4 and i < 50:
+                xy = suite(x, y, cx, cy)
+                x = xy[0]
+                y = xy[1]
+                i += 1
+            if i == 50:
+                f = open("cvalide.txt", "a")
+                f.write(str((x, y)) + ",")
+    return
 
 
 def suitex(x, y, cx):
@@ -29,8 +53,10 @@ def distom(x, y):
 
 
 def suite(x, y, cx, cy):
-    x = suitex(x, y, cx)
-    y = suitey(x, y, cy)
+    tpx=x
+    tpy=y
+    x = suitex(tpx, tpy, cx)
+    y = suitey(tpx, tpy, cy)
     return x, y
 
 
@@ -53,17 +79,15 @@ def isMandelbrot(c):
 if __name__ == "__main__":
     img = Image.new('RGB', (4000, 4000))
     pixels = img.load()
-    cs = pointc(2)
+    cs = pointc(2, 2)
     cvalide = []
     for c in cs:
-        x = int(c[0]*1000+2000)
-        y = int(c[1]*1000+2000)
+        x = int(c[0])
+        y = int(c[1])
         if isMandelbrot(c):
             cvalide.append(c)
-            pixels[x, y] = (255, 0, 0)
-
-    #fichier = open("cvalide.txt", "w")
-    #fichier.write(str(cvalide))
-
+            pixels[x, y] = (255, 255, 255)
+    fichier = open("cvalide.txt", "w")
+    fichier.write(str(cvalide))
     img.show()
 
